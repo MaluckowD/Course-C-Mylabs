230913feab9fa1a7bfd7f15eed2badac3a3e9aa5
Mitya <dim.maluckow2017@yandex.ru>
2024-10-20 13:48:02 +0300

    create function append

--- lab8/lab8.c
+++ lab8/lab8.c
@@ -35,6 +35,33 @@ void *search(int *data){
   return data;
 }
 
+void *append(int *data, int K, int N, int a_min, int b_max){
+  srand(time(NULL));
+  if ((K > data[0]) || (K <= 0))
+  {
+    return data;
+  }
+
+  int *arr = malloc(sizeof(int) * (N + data[0] + 1));
+  int count = 1;
+  for (int i = 1; i < K; i++ ){
+    arr[count++] = data[i];
+  }
+
+  for (int i = 0; i < N; i++){
+    arr[count++] = rand() % (b_max - a_min + 1) + a_min;
+  }
+
+  for (int i = K; i <= data[0]; i++){
+   arr[count++] = data[i];
+  }
+
+  arr[0] = data[0] + N;
+  free(data);
+  data = arr;
+  return data;
+}
+
 int main()
 {
 
@@ -92,13 +119,10 @@ int main()
   int K;
   printf("Input index element you want delete: \n");
   scanf("%d", &K);
-  for (int i = 0; i <= *coords[0]; i++)
-  {
-    printf("%d  ", coords[0][i]);
-  }
 
   coords[0] = delete_element(coords[0], K);
   coords[1] = search(coords[1]);
+  coords[2] = append(coords[2], 4, 2,  a_min, b_max);
   for (int i = 0; i <= coords[0][0]; i++)
   {
     printf("%d  ", coords[0][i]);
@@ -106,7 +130,10 @@ int main()
   for (int i = 0; i <= coords[1][0]; i ++){
     printf("%d ", coords[1][i]);
   }
-  
+  for (int i = 0; i <= coords[2][0]; i++)
+  {
+    printf("%d ", coords[2][i]);
+  }
 
   // Освобождение выделенной памяти
   for (int i = 0; i < A; i++)
