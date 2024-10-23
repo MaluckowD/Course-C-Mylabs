b9986a9521765194afce234e135c82313dd84ac4
Mitya <dim.maluckow2017@yandex.ru>
2024-10-23 13:16:07 +0300

    solve function add

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
 int info_count(char str[]){
   int count = 0;
@@ -51,9 +52,36 @@ int main(int argc, char *argv[])
   char str[1000];
   strcpy(str, argv[1]);
   int count = info_count(str);
+  char *ar = malloc(100 * sizeof(char));
+  
+  const char delimiters[] = ",.!? ";
+  const char glas[] = "йцкнгшщзхфвпрлджчсмтб";
+  const char sogl[] = "уеыаоэюия";
+  char *token = strtok(str, delimiters);
+
+  while (token != NULL)
+  {
+    //printf("Token: %s\n", token);
+    int count1 = 0;
+    int count2 = 0;
+    for (int i = 0; i < strlen(token); i++){
+      if (strchr(glas, token[i]) != NULL)
+      {
+        count1++;
+      }
+      if (strchr(sogl, token[i]) != NULL)
+      {
+        count2++;
+      }
+        }
+    if (count2 > count1){
+      printf("%s\n", token);
+    }
+    
+    token = strtok(NULL, delimiters);
+  }
 
   printf("%d\n", count);
   
-
   return 0;
 }
