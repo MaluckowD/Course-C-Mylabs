e69c48297e2a31d438ef935186d26a21ba0096c7
Mitya <dim.maluckow2017@yandex.ru>
2024-10-24 13:31:11 +0300

    create dynamic massive

--- lab9/lab9.c
+++ lab9/lab9.c
@@ -5,7 +5,8 @@
 #include <string.h>
 #include <ctype.h>
 
-int info_count(char str[]){
+int info_count(char str[])
+{
   int count = 0;
   char *pt = str;
   char *ptr = strpbrk(str, "!?.");
@@ -41,30 +42,52 @@ int info_count(char str[]){
   int n = strlen(str);
   if ((str[n - 1] != '!' && str[n - 1] != '?' && str[n - 1] != '.') && (str[n] == '\0'))
   {
-
     count++;
   }
   return count;
+}
+
+void create()
+{
+  // Реализуйте функцию create()
 }
 
 int main(int argc, char *argv[])
 {
   char str[1000];
   strcpy(str, argv[1]);
-  int count = info_count(str);
-  char *ar = malloc(100 * sizeof(char));
-  
+  int count = info_count(str); // Количество слов
+
+  // Выделение памяти для массива указателей на строки
+  char **ar = (char **)malloc(count * sizeof(char *));
+  if (ar == NULL)
+  {
+    return 1; // Обработка ошибки: не удалось выделить память
+  }
+
+  // Выделение памяти для каждой строки
+  for (int i = 0; i < count; i++)
+  {
+    ar[i] = (char *)malloc(100 * sizeof(char));
+    if (ar[i] == NULL)
+    {
+      return 1; // Обработка ошибки: не удалось выделить память
+    }
+  }
+
   const char delimiters[] = ",.!? ";
   const char glas[] = "йцкнгшщзхфвпрлджчсмтб";
   const char sogl[] = "уеыаоэюия";
   char *token = strtok(str, delimiters);
 
+  int c = 0; // Счетчик для массива ar
   while (token != NULL)
   {
-    //printf("Token: %s\n", token);
     int count1 = 0;
     int count2 = 0;
-    for (int i = 0; i < strlen(token); i++){
+
+    for (int i = 0; i < strlen(token); i++)
+    {
       if (strchr(glas, token[i]) != NULL)
       {
         count1++;
@@ -73,15 +96,30 @@ int main(int argc, char *argv[])
       {
         count2++;
       }
-        }
-    if (count2 > count1){
-      printf("%s\n", token);
     }
-    
+    if (count2 > count1)
+    {
+      strcpy(ar[c], token);
+      c++;
+    }
+
     token = strtok(NULL, delimiters);
   }
 
-  printf("%d\n", count);
-  
+  printf("%d\n", count); // Вывод количества слов
+
+  // Вывод слов, в которых согласных больше, чем гласных
+  for (int i = 0; i < c; i++)
+  {
+    puts(ar[i]);
+  }
+
+  // Освобождение памяти
+  for (int i = 0; i < count; i++)
+  {
+    free(ar[i]);
+  }
+  free(ar);
+
   return 0;
 }
