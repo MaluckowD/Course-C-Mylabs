6c67deae9e13e74726f0351dc2f47f84ee166b6e
Mitya <dim.maluckow2017@yandex.ru>
2024-11-03 14:48:15 +0300

    created first entity

--- lab12/lab12.c
+++ lab12/lab12.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <math.h>
 #include <string.h>
 
-enum type_device
+typedef enum type_device
 {
   Telephone,
   computer,
   printer
-};
+} DeviceType;
 
 struct Telephone_params{
   char OS;
@@ -29,16 +28,57 @@ struct printer_params
 };
 
 struct Device {
-  enum type_device type;
+  DeviceType type;
   union {
     struct Telephone_params telephone;
     struct computer_params computer;
     struct printer_params printer;
   };
+
+  enum DeviceStateFlags
+  {
+    ON = 1,
+    OFF = 2,
+    WIFI = 4,
+    BLUETOOTH = 8
+  }DeviceStateFlags;
+  unsigned int stateFlags;
 };
 
 int main(){
+  struct Device pk;
+  pk.type = computer;
+  pk.computer.count_cores = 8;
+  pk.computer.count_RAM = 128;
+  pk.stateFlags = BLUETOOTH | WIFI | ON;
 
+  printf("Компьютер:\n");
+  printf("  Тип: %d\n", pk.type);
+  printf("  Ядра: %d\n", pk.computer.count_cores);
+  printf("  RAM: %d ГБ\n", pk.computer.count_RAM);
+  printf("  Состояние: \n");
+  if (pk.stateFlags & ON)
+  {
+    printf("    Включен\n");
+  }
+  else{
+    printf("    Выключен\n");
+  }
+  
+  if (pk.stateFlags & WIFI)
+  {
+    printf("    Wi-Fi включен\n");
+  }
+  else{
+    printf("    Wi-Fi выключен\n");
+  }
+  if (pk.stateFlags & BLUETOOTH)
+  {
+    printf("    Bluetooth включен\n");
+  }
+  else{
+    printf("    Bluetooth выключен\n");
+  }
 
   return 0;
 }
